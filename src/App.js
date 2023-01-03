cf94bf5d6d042869778069145f6e7ea0ac5cac02
Luis Abellan <luis.abellan.dev@gmail.com>
2023-01-03 13:33:50 +0100

    Added navigation mdbkit5

--- src/App.js
+++ src/App.js
@@ -1,109 +1,86 @@
-import logo from './images/logo-1-300x138.png';
-
+import logo from './images/logo-1-300x138.png'
+import Navigation from './components/Navigation/Navigation'
+import Aside from './components/Aside/Aside'
 import {
+  
   MDBCarousel,
   MDBCarouselItem,
-} from 'mdb-react-ui-kit';
+} from 'mdb-react-ui-kit'
 
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import './App.scss';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './App.scss'
 
 function App() {
+
   return (
     <div id="pageContainer">
-      <header class="logoAndMenuItemsContainer">
-        <div class="menu">
-          <div class="logo">
-            <li class="nobullets"><a href="./"><img class="logo-img" src={logo}
-              alt="logo" /></a></li>
+     {/*  <header className="logoAndMenuItemsContainer">
+        <div className="menu">
+          <div className="logo">
+            <li className="nobullets">
+              <a href="./">
+                <img className="logo-img" src={logo} alt="logo" />
+              </a>
+            </li>
           </div>
-          <div class="menuItems">
+          <div className="menuItems">
             <ul>
-              <li><a href="../"><span class="orange">Inicio</span></a></li>
-              <li><a href="./servicios"><span>Servicios24</span></a></li>
-              <li><a href="./recomendamos"><span class="ini">Recomendamos</span></a></li>
-              <li><a href="./"><span>Contacto</span></a></li>
-
+              <li>
+                <a href="../">
+                  <span className="orange">Inicio</span>
+                </a>
+              </li>
+              <li>
+                <a href="./servicios">
+                  <span>Servicios24</span>
+                </a>
+              </li>
+              <li>
+                <a href="./recomendamos">
+                  <span className="ini">Recomendamos</span>
+                </a>
+              </li>
+              <li>
+                <a href="./">
+                  <span>Contacto</span>
+                </a>
+              </li>
             </ul>
           </div>
 
-          <nav id="mobileMenu" class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                data-mdb-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-              </button>
-            </div>
-
-            <div class="collapse" id="navbarToggleExternalContent">
-              <div class="bg-light shadow-3 p-4">
-                <button class="btn btn-link btn-block border-bottom m-0">
-                 {/*eslint-disable-next-line*/ }
-
-                  <a href="./"><span
-                  class="orange">Inicio</span></a></button>
-                <button class="btn btn-link btn-block border-bottom m-0"><a
-                  href="./servicios"><span>Servicios24</span></a></button>
-                <button class="btn btn-link btn-block m-0">
-                 {/*eslint-disable-next-line*/ }
-                  <a href="./recomendamos" /><span
-                  class="ini">Recomendamos</span></button>
-                <button class="btn btn-link btn-block m-0">
-                 {/*eslint-disable-next-line*/ }
-                
-                 {/*eslint-disable-next-line*/ }
-                  <a href="./contacto"/><span
-                  class="ini">Contacto</span></button>
-              </div>
-            </div>
-          </nav>
-
         </div>
+      </header> */}
+      <Navigation />
 
-
-      </header>
-      <aside class="intro-section">
-        <h2>RECOMENDACIONES PARA COCHES</h2>
-        <p><span class="orangeText">Taller 24 horas Madrid,</span> se enfoca en tu seguridad y tranquilidad a la
-          hora de transitar, encuentra las mejores recomendaciones para coches, alarga la vida de tu vehículo
-          con
-          prácticos consejos y trucos, para que se mantenga constantemente en buen estado</p>
-        {/* images */}
-
-      </aside>
+      <Aside />
 
       <MDBCarousel showControls>
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={1}
-        src='https://mdbootstrap.com/img/new/slides/041.jpg'
-        alt='...'
-      />
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={2}
-        src='https://mdbootstrap.com/img/new/slides/042.jpg'
-        alt='...'
-      />
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={3}
-        src='https://mdbootstrap.com/img/new/slides/043.jpg'
-        alt='...'
-      />
-    </MDBCarousel>
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={1}
+          src="https://mdbootstrap.com/img/new/slides/041.jpg"
+          alt="..."
+        />
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={2}
+          src="https://mdbootstrap.com/img/new/slides/042.jpg"
+          alt="..."
+        />
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={3}
+          src="https://mdbootstrap.com/img/new/slides/043.jpg"
+          alt="..."
+        />
+      </MDBCarousel>
 
-    
+      <main id="contacto"></main>
 
-      <main id="contacto">
-      </main>
-
-
-      <footer class="footer">Copyright 2020</footer>
+      <footer className="footer">Copyright 2020</footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+export default function Navigation() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Inicio
+              </MDBNavbarLink>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Servicios24
+              </MDBNavbarLink>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Recomendamos
+              </MDBNavbarLink>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Contacto
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+             {/*<MDBNavbarItem>
+              <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+            </MDBNavbarItem>
+
+            <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                  Dropdown
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem link>Inicio</MDBDropdownItem>
+                  <MDBDropdownItem link>Servicios24</MDBDropdownItem>
+                  <MDBDropdownItem link>Recomendamos</MDBDropdownItem>
+                  <MDBDropdownItem link>Contacto</MDBDropdownItem>
+                </MDBDropdownMenu> 
+              </MDBDropdown>
+            </MDBNavbarItem>*/}
+
+            {/* <MDBNavbarItem>
+              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                Disabled
+              </MDBNavbarLink>
+            </MDBNavbarItem> */}
+          </MDBNavbarNav>
+
+         {/*  <form className='d-flex input-group w-auto'>
+            <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
+            <MDBBtn color='primary'>Search</MDBBtn>
+          </form> */}
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
