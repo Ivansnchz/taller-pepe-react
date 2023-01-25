048d337c75128b225cc077fe67eb5b14fb95b98a
Luis Abellan <luis.abellan.dev@gmail.com>
2023-01-25 19:51:25 +0100

    esto es una comentario resumen
    - comer(): added term parameter
    - otraFuncion(): I did such and such
    - other comment

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -3,14 +3,14 @@ export default function Aside() {
 
   return (
     <aside className="intro-section">
-    <h2>RECOMENDACIONES PARA COCHES</h2>
-    <p>
-      <span className="orangeText">Taller 24 horas Madrid,</span> se enfoca
-      en tu seguridad y tranquilidad a la hora de transitar, encuentra las
-      mejores recomendaciones para coches, alarga la vida de tu vehículo con
-      prácticos consejos y trucos, para que se mantenga constantemente en
-      buen estado
-    </p>
+      <h2>RECOMENDACIONES PARA COCHES</h2>
+      <p>
+        <span className="orangeText">Taller 24 horas Madrid,</span> se enfoca
+        en tu seguridad y tranquilidad a la hora de transitar, encuentra las
+        mejores recomendaciones para coches, alarga la vida de tu vehículo con
+        prácticos consejos y trucos, para que se mantenga constantemente en
+        buen estado
+      </p>
   </aside>
   );
 }
